9a3aff212f901c65788ae8a6608cee5b80a3bf0f
ourgye <cadmium4488@gmail.com>
2024-10-27 20:32:05 +0900

    design: tab button and floating

--- components/common/FloatingCreateRecordButton.tsx
+++ components/common/FloatingCreateRecordButton.tsx
@@ -6,9 +6,13 @@ import colors from "@/assets/colors/colors";
 
 import styles from "./style/FloatingCreateRecordButton";
 
-export function FloatingCreateRecordButton({from}: {from: string}) {
+export function FloatingCreateRecordButton({ from }: { from: string }) {
   return (
-    <Link href={from + "/create-record"} asChild style={styles.addFloatingButton}>
+    <Link
+      href={from + "/create-record"}
+      asChild
+      style={styles.addFloatingButton}
+    >
       <Pressable>
         <SvgIcon name="Floating_add_icon" size={50} fill={colors.white0} />
       </Pressable>
--- components/common/style/FloatingCreateRecordButton.ts
+++ components/common/style/FloatingCreateRecordButton.ts
@@ -3,13 +3,12 @@ import colors from "@/assets/colors/colors";
 
 const styles = StyleSheet.create({
   addFloatingButton: {
+    zIndex: 100,
     width: 56,
     height: 56,
-    zIndex: 100,
-    backgroundColor: colors.blue0,
     position: "absolute",
     right: 24,
-    bottom: 24,
+    bottom: 98,
     borderRadius: 28,
     borderTopLeftRadius: 0,
     justifyContent: "center",
