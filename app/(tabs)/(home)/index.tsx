ef35787829fef86324e06d3d33ea01dc4c6e960a
ourgye <cadmium4488@gmail.com>
2024-11-09 17:23:57 +0900

    fix: 메인 페이지 아카이브 부분 컴포넌트 개략

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -2,14 +2,17 @@ import { StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HomeList } from "@/components/home/HomeList";
 import { FloatingCreateRecordButton } from "@/components/common/FloatingCreateRecordButton";
+import { HomeCalendar } from "@/components/home/HomeCalendar";
+import { ScrollView } from "react-native-gesture-handler";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container} edges={["right", "top", "left"]}>
       <FloatingCreateRecordButton from="(home)" />
-      <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <HomeCalendar />
         <HomeList />
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
