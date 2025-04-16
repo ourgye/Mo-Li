b910dfbe4b50443777326d03daea1cca5ae12157
ourgye <cadmium4488@gmail.com>
2025-04-16 13:36:10 +0900

    chore: delete unused files

--- constants/MyPage.ts
+++
@@ -1,8 +0,0 @@
-import { MyPageListType } from "./types.interface";
-
-const myPageList: MyPageListType[] = [
-  { _id: 0, name: "아카이브 관리", path: "/(tabs)/(setting)/archive-manage" },
-  { _id: 2, name: "도움말", path: "/(tabs)/(setting)/help" },
-];
-
-export default myPageList;
